74357a1418af424780045d5ad04b0d58aec6a7e0
denonmusic <denizonermusic@gmail.com>
2025-05-15 02:44:07 +0300

    Update setup.py
    ajda

--- setup.py
+++ setup.py
@@ -18,3 +18,4 @@ setup(
     ],
     include_package_data=True
 )
+python jukebox/sample.py --model=5b_lyrics --name=yeniden_baslasin_nu_disco --lyrics="Ajda Pekkan yeniden başlasın" --genre="nu-disco" --levels=3 --sample_length_in_seconds=30 --total_sample_length_in_seconds=180 --sr=44100
